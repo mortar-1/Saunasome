0abffca89fdba5450cbd493149f785748a6f2835
mortar <ariel.mortengren@gmail.com>
2021-08-25 15:37:13 +0300

    uutta pukkaa

--- src/main/java/projekti/PhotoService.java
+++ src/main/java/projekti/PhotoService.java
@@ -13,7 +13,6 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
---
+++ src/main/resources/public/javascript/navbar.js
@@ -0,0 +1,8 @@
+function openOrCloseSidebar() {
+
+    if(document.getElementById("navbar").style.display=="none") {
+            document.getElementById("navbar").style.display = "block";
+    } else {
+           document.getElementById("navbar").style.display = "none";
+    }
+}
