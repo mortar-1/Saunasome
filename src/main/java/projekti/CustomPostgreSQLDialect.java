b72280746017bbc903cd017d01eac13f68bb31cd
mortar <ariel.mortengren@gmail.com>
2021-08-26 18:11:26 +0300

    oid -> bytea

--- src/main/java/projekti/CustomPostgreSQLDialect.java
+++ src/main/java/projekti/CustomPostgreSQLDialect.java
@@ -1,10 +1,17 @@
 package projekti;
 
+import java.sql.Types;
 import org.hibernate.dialect.PostgreSQL82Dialect;
 import org.hibernate.type.descriptor.sql.BinaryTypeDescriptor;
 import org.hibernate.type.descriptor.sql.SqlTypeDescriptor;
 
 public class CustomPostgreSQLDialect extends PostgreSQL82Dialect {
+
+    public CustomPostgreSQLDialect() {
+                
+        registerColumnType(Types.BLOB, "bytea");
+
+    }
 
     @Override
     public SqlTypeDescriptor remapSqlTypeDescriptor(SqlTypeDescriptor sqlTypeDescriptor) {
